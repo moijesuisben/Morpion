0f3b072461f338962d2a6eb5a66569ea44176c50
Benjamin Arpage <benji.apg@gmail.com>
2018-10-17 13:10:39 +0200

    MàJ 17_10 13:10

--- js/script.js
+++ js/script.js
@@ -7,28 +7,28 @@ var afficherSymbole = function(cell) {
 	// 1 - verifier case remplie ou pas
 	if (cell.textContent === '') {
 		// 2 - poser symbole J1 ou j2
-		if (tourDuJoueur1) {
-			cell.textContent = 'X';
+    if (tourDuJoueur1) {
+			cell.classList.add('rouge');
 		} else {
-			cell.textContent = 'O';
+			cell.classList.add('bleu');
 		}
 		// 4 - changer le joueur courant
 		tourDuJoueur1 = !tourDuJoueur1;
 	}
 };
 
-var combinaisons = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+var combinaisons = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 var verifierCombinaisons = function() {
 	// a remplir
 	// 3 - check combinaison gagnante
 	combinaisons.forEach(function(combinaison) {
 		if (
-			cells[combinaison[0]].textContent === cells[combinaison[1]].textContent &&
-			cells[combinaison[1]].textContent === cells[combinaison[2]].textContent &&
-			cells[combinaison[0]].textContent !== ''
+			cells[combinaison[0]].classList[0] === cells[combinaison[1]].classList[1] &&
+			cells[combinaison[1]].classList[1] === cells[combinaison[2]].classList[2] &&
+			cells[combinaison[0]].classList[0] !== ''
 		) {
-			console.log('WIN');
+			console.log("T'as gagné bravo");
 			var currentPlayer;
 			if (tourDuJoueur1) {
 				currentPlayer = 'joueur 2';
